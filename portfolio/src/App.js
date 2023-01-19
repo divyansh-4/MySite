10d1ecbbc81138b73666a1853d2ad092781686c5
Divyansh <divyansh.72003@gmail.com>
2023-01-19 10:36:29 +0530

    Created the navbar and added modes for mobile site

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./index.css";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Project from "./routes/Project";
+import Contact from "./routes/Contact";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <div>
-      <h1>This is the Portfolio </h1>
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/project" element={<Project/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+    </Routes>
+    </>
   );
 }
 
